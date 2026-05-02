c102ea71f38f1f647230acd246362ab4d163d76f
Benjamin N. <63211473+bnamayandev@users.noreply.github.com>
2026-05-02 18:27:34 -0400

    Add: pacific-atlantic-water-flow - submission-0

---
+++ Data Structures & Algorithms/pacific-atlantic-water-flow/submission-0.py
@@ -0,0 +1,30 @@
+class Solution:
+    def pacificAtlantic(self, heights: List[List[int]]) -> List[List[int]]:
+        m, n = len(heights), len(heights[0])
+        touchP = set()
+        touchA = set()
+
+        def dfs(r,c,currHeight, ocean):
+            if r < 0 or c < 0 or r >= m or c >= n or (r,c) in ocean:
+                return
+            
+            if heights[r][c] >= currHeight:
+                ocean.add((r,c))
+                dfs(r - 1,c, heights[r][c], ocean)
+                dfs(r + 1,c, heights[r][c], ocean)
+                dfs(r,c - 1, heights[r][c], ocean)
+                dfs(r,c + 1, heights[r][c], ocean)
+            
+            else:
+                return
+            
+
+
+        for r in range(m):
+            for c in range(n):
+                if r == 0 or c == 0:
+                    dfs(r,c, -1, touchP)
+                if r == m - 1 or c == n - 1:
+                    dfs(r,c, 0, touchA)
+
+        return [(r, c) for (r, c) in touchP & touchA]
